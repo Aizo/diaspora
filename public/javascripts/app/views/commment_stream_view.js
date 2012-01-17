d1f37b094caf4d63908d654ef44ffdaa659231d5
danielgrippi <danielgrippi@gmail.com>
2012-01-17 10:04:27 -0800

    draft1

--- public/javascripts/app/views/commment_stream_view.js
+++ public/javascripts/app/views/commment_stream_view.js
@@ -34,7 +34,7 @@ app.views.CommentStream = app.views.Base.extend({
   },
 
   appendComment: function(comment) {
-    this.$("ul.comments").append(new app.views.Comment({
+    this.$(".comments").append(new app.views.Comment({
       model: comment
     }).render().el);
   },
